50110ddac9d2e78ea6bd0914af78bf78223f2cfe
tornikeo <tornikeonoprishvili@gmail.com>
2022-03-14 22:57:32 +0400

    Fix deployment action not coying over files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,7 @@
+git checkout -b gh-pages
 mv dist/* .
+git config --global user.name 'GoodBot'
+git config --global user.email 'GoodBot@bots.noreply.github.com'
+git add .
+git commit -am "Auto update gh-pages"
+git push -f origin gh-pages
