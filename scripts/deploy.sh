88ce264963881695f6d120f365e40292791bb00e
tornikeo <tornikeonoprishvili@gmail.com>
2022-03-14 23:02:20 +0400

    Fix deployment action not updating

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
-git checkout -b gh-pages
 mv dist/* .
 git config --global user.name 'GoodBot'
 git config --global user.email 'GoodBot@bots.noreply.github.com'
 git add .
 git commit -am "Auto update gh-pages"
-git push -f origin gh-pages
+git push -f origin HEAD:gh-pages
